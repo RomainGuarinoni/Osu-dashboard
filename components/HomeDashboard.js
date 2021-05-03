10b24514444291620cb1f7dd63ee04bc6c0228dc
RomainGuarinoni <romain.guar91@gmail.com>
2021-05-03 13:20:46 +0200

    add home components and clear useless folder

---
+++ components/HomeDashboard.js
@@ -0,0 +1,5 @@
+import style from "../styles/HomeDashboard.module.css";
+
+export default function HomeDashboard() {
+  return <div className={style.container}></div>;
+}
---
+++ components/HomeProfil.js
@@ -0,0 +1,4 @@
+import style from "../styles/HomeProfil.module.css";
+export default function HomeProfil() {
+  return <div className={style.container}></div>;
+}
