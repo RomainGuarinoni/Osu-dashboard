fb9b3e5569bd6158d64e906035daf4a669cc78d7
RomainGuarinoni <romain.guar91@gmail.com>
2021-05-03 14:18:17 +0200

    set up dashboard home page

--- components/HomeDashboard.js
+++ components/HomeDashboard.js
@@ -1,5 +1,25 @@
 import style from "../styles/HomeDashboard.module.css";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faGlobeAmericas } from "@fortawesome/free-solid-svg-icons";
 export default function HomeDashboard() {
-  return <div className={style.container}></div>;
+  return (
+    <div className={style.container}>
+      <header className={style.header}>
+        <h1 className={style.title}>OSU DASHBOARD</h1>
+      </header>
+      <div className={style.graphContainer}></div>
+      <footer className={style.footer}>
+        <a
+          href="https://github.com/RomainGuarinoni/Osu-dashboard"
+          target="__blank"
+        >
+          <FontAwesomeIcon className={style.brandIcon} icon={faGithub} />
+        </a>
+        <a href="https://osu.ppy.sh/home" target="__blank">
+          <FontAwesomeIcon className={style.brandIcon} icon={faGlobeAmericas} />
+        </a>
+      </footer>
+    </div>
+  );
 }
