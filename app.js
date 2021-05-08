10d5e6b9964c9779d0febc979b4fb5b780a2b0e6
RomainGuarinoni <romain.guar91@gmail.com>
2021-05-08 21:31:14 +0200

    add route for recent activity

--- app.js
+++ app.js
@@ -151,11 +151,45 @@ app.get("/getGraph/:userID/:token", (req, res, next) => {
       objectResponse["recentDifficulty"] = recentDifficulty.reverse();
       objectResponse["labels"] = labels.reverse();
       console.log(recentMod);
-      res.status(200).json(objectResponse);
+      return axios({
+        url: `https://osu.ppy.sh/api/v2/users/${user}/recent_activity`,
+        method: "get",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
     })
     .catch((err) => {
       res.status(200).json("there is an error");
       console.log(`error 2 :${err}`);
+    })
+    .then((response) => {
+      let topPlaces = {
+        top50: 0,
+        top100: 0,
+        top250: 0,
+        top500: 0,
+        top1000: 0,
+      };
+      response.data.forEach((element) => {
+        if (element.rank <= 50) {
+          topPlaces["top50"] += 1;
+        } else if (element.rank <= 100) {
+          topPlaces["top100"] += 1;
+        } else if (element.rank <= 250) {
+          topPlaces["top250"] += 1;
+        } else if (element.rank <= 500) {
+          topPlaces["top500"] += 1;
+        } else if (element.rank <= 1000) {
+          topPlaces["top1000"] += 1;
+        }
+      });
+      objectResponse["topPlaces"] = topPlaces;
+      res.status(200).json(objectResponse);
+    })
+    .catch((err) => {
+      console.log(`error 3  : ${err}`);
+      res.status(400).json("an error has occured");
     });
 });
 
