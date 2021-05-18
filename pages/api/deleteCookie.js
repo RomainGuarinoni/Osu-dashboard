878aa35bbe9126416b2ab93db4c384ddb7bd98dc
RomainGuarinoni <romain.guar91@gmail.com>
2021-05-18 23:42:54 +0200

    feat(API): add HTPP-ONLY route for token

---
+++ pages/api/deleteCookie.js
@@ -0,0 +1,16 @@
+import cookie from "cookie";
+/*req => {
+    key : String,
+}*/
+export default function handler(req, res) {
+  res.setHeader(
+    "Set-Cookie",
+    cookie.serialize(req.body.key, "", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      maxAge: new Date(0),
+      sameSite: "strict",
+      path: "/",
+    })
+  );
+}
---
+++ pages/api/setCookie.js
@@ -0,0 +1,20 @@
+import cookie from "cookie";
+
+//function that set a HHTP only cookie
+/*req => {
+    key : String,
+    value : String
+}*/
+export default function handler(req, res) {
+  res.setHeader(
+    "Set-Cookie",
+    cookie.serialize(req.body.key, req.body.value, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      maxAge: 86400,
+      sameSite: "strict",
+      path: "/",
+    })
+  );
+  res.status(200).json({ msg: "cookie has been set" });
+}
