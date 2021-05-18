03ec5bf359fd69483b36c1fda1d788ec4f9ff540
RomainGuarinoni <romain.guar91@gmail.com>
2021-05-18 20:01:46 +0200

    build(functions): add getUrlParams

---
+++ function/getUrlParam.js
@@ -0,0 +1,8 @@
+import { useRouter } from "next/router";
+export function getUrlParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const paramValue = urlParams.get(param);
+
+  return paramValue;
+}
